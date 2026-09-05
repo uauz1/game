5d5b2135a5e02713ab5b2a9365ca2507d245e29b
uauz1 <uauz99@gmail.com>
2026-09-05 20:57:10 +0300

    Add unified Qaddha site shell

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -4,12 +4,18 @@ import { BottomNav } from './BottomNav';
 
 export function Layout() {
   return (
-    <div className="min-h-screen flex flex-col">
-      <Navbar />
-      <main className="flex-1 pb-20 lg:pb-0">
-        <Outlet />
-      </main>
-      <BottomNav />
+    <div className="relative min-h-screen overflow-x-hidden bg-[#07090d] text-off-white">
+      <div className="pointer-events-none fixed inset-0 -z-10">
+        <div className="absolute -right-40 top-24 h-[460px] w-[460px] rounded-full bg-[#f6b94f]/[.045] blur-3xl" />
+        <div className="absolute -left-40 bottom-20 h-[420px] w-[420px] rounded-full bg-[#f6b94f]/[.03] blur-3xl" />
+      </div>
+      <div className="flex min-h-screen flex-col">
+        <Navbar />
+        <main className="flex-1 pb-20 lg:pb-0">
+          <Outlet />
+        </main>
+        <BottomNav />
+      </div>
     </div>
   );
 }
