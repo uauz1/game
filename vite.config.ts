39de6f79cdfc004a384ce7baea44da9f2174e22d
uauz1 <uauz99@gmail.com>
2026-09-05 19:47:52 +0300

    Configure Vite base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { fileURLToPath, URL } from 'node:url';
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.GITHUB_ACTIONS ? '/game/' : '/',
   plugins: [react()],
   resolve: {
     alias: {
