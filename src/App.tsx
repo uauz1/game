60a6600fca075cfe193854193b1ba64f56a03aed
uauz1 <uauz99@gmail.com>
2026-09-05 19:47:47 +0300

    Prepare routing for GitHub Pages

--- src/App.tsx
+++ src/App.tsx
@@ -26,7 +26,7 @@ export default function App() {
       <ToastProvider>
         <FavoritesProvider>
           <GameProvider>
-            <BrowserRouter>
+            <BrowserRouter basename={import.meta.env.BASE_URL}>
               <Routes>
                 <Route element={<Layout />}>
                   <Route path="/" element={<Home />} />
