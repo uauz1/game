1b5668682cd86183467ac8da114a898d07204200
uauz1 <uauz99@gmail.com>
2026-09-06 04:31:50 +0300

    Remove alternate game design styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,6 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import './letters.css';
-import './letters-reference.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
